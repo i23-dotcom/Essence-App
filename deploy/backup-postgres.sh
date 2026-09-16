1f7c03a660157d63297d874362f38cae16712137
i23-dotcom <123354604+i23-dotcom@users.noreply.github.com>
2026-09-16 16:47:26 +0300

    Delete deploy directory

--- deploy/backup-postgres.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eu
-mkdir -p backups
-STAMP=$(date +"%Y%m%d-%H%M%S")
-pg_dump "$DATABASE_URL" | gzip > "backups/essence_network_$STAMP.sql.gz"
-echo "Backup written to backups/essence_network_$STAMP.sql.gz"
